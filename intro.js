0f18bdaef897818794997f24137fa1d507ac7131
sarthakc7 <sarthak.c@capitalfloat.com>
2022-04-06 21:29:33 +0530

    routing in combo with fs.readFile

--- intro.js
+++ intro.js
@@ -1,46 +1,30 @@
-const { appendFile } = require('fs');
 const http = require('http');
+const fs = require('fs');
 
 let routes = {
-    "/": "Home",
-    "/about": "About",
-    "/contact": "Contact",
-};
+    "/": "index.html",
+    "/about": "about.html",
+    "/contact": "contact.html",
+    "/demo": "demoHTML.htm"
+    "/blog": "blog.html" // invalid route as file doesn't exist-> return 404
+}
 
 //create a server object:
 const app = http.createServer(function (req, res) {
 
-    // http header
-
-    var url = req.url;
-
-    if (routes[url]) { // if url is in routes 
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.end(routes[url]);
-    }
-    else {
-        res.writeHead(404, { 'Content-Type': 'text/html' });
-        res.end("Error - not found");
+    if (routes[req.url]) {
+        fs.readFile(routes, (err, data) => {
+            if (err) {                
+                res.writeHead(404, "File Not Found");
+                res.end();
+            }
+            else {
+                res.writeHead(200, { 'Content-Type': 'text/html' });
+                res.end(data);
+            }
+        })
     }
 
 }).listen(3000, function () {
     console.log("server start at port 3000"); //the server object listens on port 3000
-});
-
-app.on('request', (req, res) => {
-    let body = [];
-
-    req.on('data', (bodyData) => {
-        body.push(bodyData);
-    });
-
-    req.on('end', () => {
-        body = Buffer.concat(body).toString();
-        console.log(body);
-    });
-
-    console.log(`Method : ${JSON.stringify(req.method)}`);
-    console.log(`Url : ${JSON.stringify(req.url)}`);
-    console.log(`Headers : ${JSON.stringify(req.headers)}`);
-
 });

c388dd44153a0e31605a2bacca7a0ccdaa895c86
sarthakc7 <sarthak.c@capitalfloat.com>
2022-04-06 21:12:04 +0530

    app eventhandlers

--- intro.js
+++ intro.js
@@ -1,18 +1,46 @@
-var http = require('http');
+const { appendFile } = require('fs');
+const http = require('http');
+
+let routes = {
+    "/": "Home",
+    "/about": "About",
+    "/contact": "Contact",
+};
+
 //create a server object:
-http.createServer(function (req, res) {
- res.writeHead(200, {'Content-Type': 'text/html'}); // http header
-var url = req.url;
- if(url ==='/about'){
-    res.write('<h1>about us page<h1>'); //write a response
-    res.end(); //end the response
- }else if(url ==='/contact'){
-    res.write('<h1>contact us page<h1>'); //write a response
-    res.end(); //end the response
- }else{
-    res.write('<h1>Hello World!<h1>'); //write a response
-    res.end(); //end the response
- }
-}).listen(3000, function(){
- console.log("server start at port 3000"); //the server object listens on port 3000
+const app = http.createServer(function (req, res) {
+
+    // http header
+
+    var url = req.url;
+
+    if (routes[url]) { // if url is in routes 
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end(routes[url]);
+    }
+    else {
+        res.writeHead(404, { 'Content-Type': 'text/html' });
+        res.end("Error - not found");
+    }
+
+}).listen(3000, function () {
+    console.log("server start at port 3000"); //the server object listens on port 3000
+});
+
+app.on('request', (req, res) => {
+    let body = [];
+
+    req.on('data', (bodyData) => {
+        body.push(bodyData);
+    });
+
+    req.on('end', () => {
+        body = Buffer.concat(body).toString();
+        console.log(body);
+    });
+
+    console.log(`Method : ${JSON.stringify(req.method)}`);
+    console.log(`Url : ${JSON.stringify(req.url)}`);
+    console.log(`Headers : ${JSON.stringify(req.headers)}`);
+
 });

d2a6917a01603ba3758fbe13c427bbec08d1eb03
sarthakc7 <sarthak.c@capitalfloat.com>
2022-04-07 20:23:13 +0530

    code related to fs methods

--- intro.js
+++ intro.js
@@ -9,12 +9,58 @@ let routes = {
     "/blog": "blog.html" // invalid route as file doesn't exist-> return 404
 }
 
+
+// fs.appendFile("POLL1.html", "POLL ANS", function (err) {
+//     if (err) {
+//         console.log('Error writing file: ' + err);
+//         throw err;
+//     }
+//     console.log("Saved!");
+// });
+
+// fs.open("demoNEW3.html", "w", function (err, file) {
+//     if (err) {
+//         console.log('Error opening file: ' + err);
+//         throw err;
+//     }
+//     console.log('file opened');    
+// });
+
+
+// fs.writeFile("NewFileWrite.txt", data, function (err) {
+//     if (err) {
+//         console.log('Error writing file: ' + err);
+//         throw err;
+//     }
+//     console.log("Write Complete!");
+// });
+
+// fs.unlink("DEMONEW2.html", function (err) {
+//     if (err) {
+//         console.log('Error deleting file: ' + err);
+//         throw err;
+//     }
+//     console.log("File deleted!");
+// });
+
+
+fs.rename("demoNEW3.html", "demoNEW-11111111111.html", function (err) {
+    if (err) {
+        console.log('Error renaming file: ' + err);
+        throw err;
+    }
+    console.log("File renamed!");
+});
+
+
+
 //create a server object:
 const app = http.createServer(function (req, res) {
 
+
     if (routes[req.url]) {
         fs.readFile(routes[req.url], (err, data) => {
-            if (err) {                
+            if (err) {
                 res.writeHead(404, "File Not Found");
                 res.end();
             }
